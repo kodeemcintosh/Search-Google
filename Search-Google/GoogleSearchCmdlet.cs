0f2816d4386f4ebd7aa255ffca5015c95bb00579
kvmac <kodee.mcintosh@gmail.com>
2017-08-27 16:10:32 -0400

    SG01: Initial commit

---
+++ Search-Google/GoogleSearchCmdlet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search_Google
+{
+	[Cmdlet(VerbsCommunications.Send, "https://www.google.com/search?q=")]
+    public class GoogleSearchCmdlet : Cmdlet
+    {
+		[Parameter]
+		public string SearchString { get; set; }
+
+		private void search(string url)
+	    {
+
+	    }
+}
